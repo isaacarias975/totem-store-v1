a5bc2122f7f409c522ec1e2ae383e85cdb85f84e
Fernando Medina Cantón <fernandomedinacanton@MacBook-Pro-de-Fernando.local>
2019-11-12 20:45:10 -0600

    desplegando aplicacion tt

---
+++ precache-manifest.3677f4abe5d5aafe6a5dc9cad4db35de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf6772e7509b6b5557e9d3e7d8236066",
+    "url": "/totem-store-v1/index.html"
+  },
+  {
+    "revision": "b617c675d219fb090c41",
+    "url": "/totem-store-v1/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "1d3569c81d0a71025f49",
+    "url": "/totem-store-v1/static/css/main.52ab31ae.chunk.css"
+  },
+  {
+    "revision": "b617c675d219fb090c41",
+    "url": "/totem-store-v1/static/js/2.0f5e878e.chunk.js"
+  },
+  {
+    "revision": "1d3569c81d0a71025f49",
+    "url": "/totem-store-v1/static/js/main.1ffa3a1a.chunk.js"
+  },
+  {
+    "revision": "774d5c7b93c6108e369b",
+    "url": "/totem-store-v1/static/js/runtime~main.59713539.js"
+  },
+  {
+    "revision": "6f35ff89a19e2d1a6ed665e6e09f4fb9",
+    "url": "/totem-store-v1/static/media/profile.6f35ff89.jpg"
+  }
+]);
